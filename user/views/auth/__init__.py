0ff5acca84d6848bcc0e656037595d711ef2d719
leonard <leonard@idb.com.vn>
2025-02-20 23:31:29 +0700

    add user app

--- user/views/auth/__init__.py
+++ user/views/auth/__init__.py
@@ -1,7 +1,6 @@
 from .login_view import LoginView
 from .logout_view import LogoutView
 from .profile_view import ProfileView
-from .register_view import RegisterView
 from .update_view import UpdateProfileView
 
 __all__ = [
