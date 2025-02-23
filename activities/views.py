340980de7bc65aa468b21f9494349fa2acdadc07
kien1910 <trungkiencntt516@gmail.com>
2025-02-23 21:01:34 +0700

    add paginator

--- activities/views.py
+++ activities/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
@@ -20,8 +21,22 @@ def activities(request: HttpRequest) -> HttpResponse:
     if not request.headers.get("HX-Request"):
         return redirect("base:404")
     activities = Activities.objects.all()
+    page_number = request.GET.get("page", 1)  # Lấy số trang từ query parameter
+    paginator = Paginator(activities, 1)  # 10 items mỗi trang
+    page_obj = paginator.get_page(page_number)
+
+    try:
+        activities_page = paginator.page(page_number)
+    except Exception:
+        activities_page = paginator.page(1)  # Nếu lỗi, quay về trang đầu tiên
+
     return render(
         request,
         "home/activities/activities.html",
-        {"activities": activities},
+        {
+            "activities": activities_page,
+            "previous_page_number": page_obj.previous_page_number()
+            if page_obj.has_previous()
+            else None,
+        },
     )
