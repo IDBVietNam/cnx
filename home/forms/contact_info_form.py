f8369d91edfb0d0af9cb3b2ceb8935485620cbdf
Ngo Anh <nvanh31798@gmail.com>
2025-02-24 14:41:43 +0700

    fix pre-commit format

--- home/forms/contact_info_form.py
+++ home/forms/contact_info_form.py
@@ -6,7 +6,9 @@ from home.models.contact_info import ContactInfo
 class ContactInfoForm(forms.ModelForm):
     loan_id = forms.CharField(required=False, label="Loan ID")
     contract_number = forms.CharField(required=True, label="Số hợp đồng")
-    contract_date = forms.DateField(required=False, label="Contract Date",input_formats=["%d/%m/%Y"])
+    contract_date = forms.DateField(
+        required=False, label="Contract Date", input_formats=["%d/%m/%Y"]
+    )
     current_account = forms.CharField(required=False, label="Current Account")
     dpd_current = forms.IntegerField(required=False, label="DPD Current")
     dpd_assign = forms.IntegerField(label="DPD Assign")
@@ -15,9 +17,11 @@ class ContactInfoForm(forms.ModelForm):
     loan_term = forms.IntegerField(required=True, label="Số kỳ hạn vay")
     obs_due_no = forms.IntegerField(required=False, label="OBS Due No")
     payment_request_date = forms.DateField(
-        required=False, label="Ngày đề nghị thanh toán",input_formats=["%d/%m/%Y"]
+        required=False, label="Ngày đề nghị thanh toán", input_formats=["%d/%m/%Y"]
     )
-    assign_invalid_date = forms.DateField(required=False, label="Assign Invalid Date",input_formats=["%d/%m/%Y"])
+    assign_invalid_date = forms.DateField(
+        required=False, label="Assign Invalid Date", input_formats=["%d/%m/%Y"]
+    )
     penalty_amount = forms.CharField(required=False, label="Số tiền phạt phát sinh")
     interest_amount = forms.CharField(required=False, label="Số tiền lãi phát sinh")
     principal_amount = forms.CharField(
@@ -27,8 +31,12 @@ class ContactInfoForm(forms.ModelForm):
     total_payment = forms.CharField(required=True, label="Tổng số tiền thanh toán")
     last_result_final = forms.CharField(required=False, label="Last Result Final")
     total_amount = forms.CharField(required=False, label="Total_Amount")
-    date_start = forms.DateField(required=False, label="Date Start",input_formats=["%d/%m/%Y"])
-    date_end = forms.DateField(required=False, label="Date End",input_formats=["%d/%m/%Y"])
+    date_start = forms.DateField(
+        required=False, label="Date Start", input_formats=["%d/%m/%Y"]
+    )
+    date_end = forms.DateField(
+        required=False, label="Date End", input_formats=["%d/%m/%Y"]
+    )
     late_days = forms.IntegerField(required=False, label="Số ngày trễ hạn")
     monthly_payment = forms.IntegerField(
         required=False, label="Số tiền phải đóng hàng tháng"
