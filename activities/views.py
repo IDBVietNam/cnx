a72d51a46c0032e92ecec85bead0a16f84e0a77c
kien1910 <trungkiencntt516@gmail.com>
2025-02-24 21:50:52 +0700

    update file pre-commit

--- activities/views.py
+++ activities/views.py
@@ -21,14 +21,14 @@ def activities(request: HttpRequest) -> HttpResponse:
     if not request.headers.get("HX-Request"):
         return redirect("base:404")
     activities = Activities.objects.all()
-    page_number = request.GET.get("page", 1)  # Lấy số trang từ query parameter
-    paginator = Paginator(activities, 1)  # 10 items mỗi trang
+    page_number = request.GET.get("page", 1)
+    paginator = Paginator(activities, 1)
     page_obj = paginator.get_page(page_number)
 
     try:
         activities_page = paginator.page(page_number)
     except Exception:
-        activities_page = paginator.page(1)  # Nếu lỗi, quay về trang đầu tiên
+        activities_page = paginator.page(1)
 
     return render(
         request,
