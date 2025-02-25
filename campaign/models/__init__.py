6f8248d4e8826c84ea3bea9ce96092073e712d7e
leonard <leonard@idb.com.vn>
2025-02-25 15:41:40 +0700

    update login page

--- campaign/models/__init__.py
+++ campaign/models/__init__.py
@@ -4,3 +4,12 @@ from .customer_info import CustomerInfo
 from .payment_info import PaymentInfo
 from .phone_info import PhoneInfo
 from .product_info import ProductInfo
+
+__all__ = [
+    "AddressInfo",
+    "ContactInfo",
+    "CustomerInfo",
+    "PaymentInfo",
+    "PhoneInfo",
+    "ProductInfo",
+]
