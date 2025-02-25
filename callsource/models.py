a589cbae8f18f1dc679990020a58e03d624340e0
kien1910 <trungkiencntt516@gmail.com>
2025-02-25 21:44:09 +0700

    update model, UI

--- callsource/models.py
+++ callsource/models.py
@@ -5,8 +5,22 @@ from cnx_service.model import BaseModel
 
 # Create your models here.
 class CallSource(BaseModel):
+    CATEGORY_CHOICES = [
+        ("hotline", "Hotline"),
+        ("community", "Community"),
+        ("digital", "Digital"),
+        ("crm_age", "CRM Age"),
+        ("retention", "Retention"),
+        ("etc", "ETC"),
+        ("referral", "Referral"),
+    ]
+
     name_call_source = models.CharField(max_length=255, null=True, blank=True)
     total_customers = models.IntegerField(null=True, blank=True)
+    note = models.TextField(null=True, blank=True)
+    category = models.CharField(
+        max_length=50, choices=CATEGORY_CHOICES, null=True, blank=True
+    )
     create_by = models.CharField(max_length=255, null=True, blank=True)
 
     class Meta:
