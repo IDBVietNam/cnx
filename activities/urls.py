57844fd74007f6e2910aee59609cceb0ef44efe3
ngochuy317 <alan@idb.com.vn>
2025-02-26 17:24:02 +0700

    add new section for testing telesale

--- activities/urls.py
+++ activities/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from activities.views import activities_form
+from activities.views import activities_form, test_telesales
 
 app_name = "activities"
 
-urlpatterns = [path("index/", activities_form, name="activities_form")]
+urlpatterns = [
+    path("index/", activities_form, name="activities_form"),
+    path("test-telesales/", test_telesales, name="test-telesales"),
+]
