f7217ff4db1997a6f826c7741a3be1f74530ec0b
ngochuy317 <alan@idb.com.vn>
2025-02-28 09:21:48 +0700

    refactor code

--- activities/urls.py
+++ activities/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from activities.views import activities, filter_activities
+from activities.views import activities, filter_activities, test_telesales
 
 app_name = "activities"
 
 urlpatterns = [
     path("activities/", activities, name="activities-page"),
     path("filter-activities/", filter_activities, name="filter-activities"),
+    path("test-telesales/", test_telesales, name="test-telesales"),
 ]
